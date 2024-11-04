ffe26d86d349befc6917da2b13fb5342aff8ed3a
Neutral <94402849+neutralgr@users.noreply.github.com>
2024-11-05 01:52:29 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+export default function handler(req, res) {
+    res.status(200).json({ message: 'Bot is running!' });
+}
