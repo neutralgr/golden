75d09de78d6a81767445575d2fd07f34a74d5045
Neutral <94402849+neutralgr@users.noreply.github.com>
2024-11-05 02:09:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, ActivityType } from 'discord.js';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.once('ready', () => {
     console.log('Bot is online!');
+    client.user.setActivity('Golden Market', { type: ActivityType.Watching });
 });
 
 client.on('messageCreate', message => {
@@ -12,7 +13,7 @@ client.on('messageCreate', message => {
     }
 });
 
-client.login(secrets.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN);
 
 export default function handler(req, res) {
     res.status(200).json({ message: 'Bot is running!' });
